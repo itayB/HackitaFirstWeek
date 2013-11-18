36cf0d0a8967d3d4d0924be3bdd3b03d55df86d3
Itay Bittan <itay.bittan@gmail.com>
2013-11-18 15:38:43 +0200

    update bottle assert

--- src/main.py
+++ src/main.py
@@ -1,22 +1,23 @@
 def helloWorld():
     return "Hello World"
     
+assert helloWorld() == "Hello World"
+
 def helloYou(name):
     return "Hello " + name
+
+assert helloYou("Itay") == "Hello Itay"
 
 def bottles(num):
     #tempList = range(0,num)
     #tempList.reverse()
     output = ""
     for x in range(1,num)[::-1]:
-        output += (" %i bottles of beer on the wall, %i bottles of beer." %(x,x) +
+        output += ("%i bottles of beer on the wall, %i bottles of beer." %(x,x) +
                    "Take one down, pass it around, %i bottles of beer on the wall...\n" %(x-1))
     
     return output
     
-assert helloWorld() == "Hello World"
-assert helloYou("Itay") == "Hello Itay"
-
-#assert bottles(2) =
-
-print bottles(3)
+assert bottles(3) ==  """2 bottles of beer on the wall, 2 bottles of beer.Take one down, pass it around, 1 bottles of beer on the wall...
+1 bottles of beer on the wall, 1 bottles of beer.Take one down, pass it around, 0 bottles of beer on the wall...
+"""
