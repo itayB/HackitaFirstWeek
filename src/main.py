c5b79072a4a030a8ad96819378811aa7256b8f79
Itay Bittan <itay.bittan@gmail.com>
2013-11-18 18:33:16 +0200

    gematria ex

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 def helloWorld():
     return "Hello World"
     
@@ -9,15 +11,38 @@ def helloYou(name):
 assert helloYou("Itay") == "Hello Itay"
 
 def bottles(num):
-    #tempList = range(0,num)
-    #tempList.reverse()
+    # tempList = range(0,num)
+    # tempList.reverse()
     output = ""
-    for x in range(1,num)[::-1]:
-        output += ("%i bottles of beer on the wall, %i bottles of beer." %(x,x) +
-                   "Take one down, pass it around, %i bottles of beer on the wall...\n" %(x-1))
+    for x in range(1, num)[::-1]:
+        output += ("%i bottles of beer on the wall, %i bottles of beer." % (x, x) + 
+                   "Take one down, pass it around, %i bottles of beer on the wall...\n" % (x - 1))
     
     return output
     
-assert bottles(3) ==  """2 bottles of beer on the wall, 2 bottles of beer.Take one down, pass it around, 1 bottles of beer on the wall...
+assert bottles(3) == """2 bottles of beer on the wall, 2 bottles of beer.Take one down, pass it around, 1 bottles of beer on the wall...
 1 bottles of beer on the wall, 1 bottles of beer.Take one down, pass it around, 0 bottles of beer on the wall...
 """
+
+def gematria(word):
+    mapa = {'a':1, 'b':2, 'c':3}
+    counter = 0
+    for c in word:
+        counter += mapa[c]
+    return counter
+
+assert gematria("abb") == 5
+
+from bottle import route, run, template, view, static_file
+
+@route('/static/<path:path>')
+def callback(path):
+    return static_file(path, root='static')
+
+@route('/')
+@view('home')
+def index():
+    return {}
+
+
+run(host='localhost', port=8080, reloader=True, debug=True)
