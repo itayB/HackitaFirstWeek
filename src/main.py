25a9f7cc8ec9529ac0a58f3864fcf30cc879364d
Itay Bittan <itay.bittan@gmail.com>
2013-11-21 18:04:26 +0200

    letterCount ex, palindromes ex, mostCommonWord ex, htmlList ex

--- src/main.py
+++ src/main.py
@@ -41,11 +41,50 @@ assert palindrome("ima") == False
     
 
 def multiplication(n):
-    return [[x * y for x in range(1,n + 1)] for y in range(1,n + 1)]
+    return [[x * y for x in range(1, n + 1)] for y in range(1, n + 1)]
 
-assert multiplication(3) == [[1,2,3],[2,4,6],[3,6,9]]
+assert multiplication(3) == [[1, 2, 3], [2, 4, 6], [3, 6, 9]]
 
-    
+def letterCount(word):
+    dic = {}
+    for x in word:
+        dic[x] = dic.get(x, 0) + 1
+        
+    return dic
+
+assert letterCount("abbcccdddd") == {'a':1, 'b':2, 'c':3, 'd':4}
+
+def palindromes(text):
+    return [x for x in text.split() if palindrome(x)]
+
+assert palindromes("aba bittib blabla") == ['aba', 'bittib']
+
+def mostCommonWord(text):
+    dic = {}
+    for word in text.split():
+        dic[word] = dic.get(word, 0) + 1
+
+    return max(dic, key=dic.get)
+
+assert mostCommonWord("Toady is my first day with Python. Python is just a great programming language") == "is"
+
+def htmlList(text, ordered):
+    if ordered: 
+        html = "<ol>" 
+    else: 
+        html = "<ul>"
+    for line in text.split('\n'):
+        html += "<li>%s</li>" % line
+    if ordered: 
+        html += "</ol>" 
+    else: 
+        html += "</ul>"   
+    return html
+
+assert htmlList("a\nb\nc\n", True) == "<ol><li>a</li><li>b</li><li>c</li><li></li></ol>"
+
+
+
 from bottle import route, run, template, view, static_file
 
 @route('/static/<path:path>')
